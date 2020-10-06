5efce51d0d9057aa5b41c14ae606ffcc015580ed
muhadimulyana <muhammadadimulyana30@gmail.com>
2020-10-06 16:58:39 +0700

    add management fixed

---
+++ application/views/management/index.php
@@ -0,0 +1,138 @@
+<!-- Hero Start -->
+<section class="bg-half d-table w-100"
+	style="background: url('<?= base_url(); ?>assets/images/home/a.jpg') center center; background-size: cover;">
+	<div class="bg-overlay"></div>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-lg-12 text-center">
+				<div class="page-next-level">
+					<h4 class="title text-white title-dark"> Human Resource </h4>
+					<div class="page-next">
+						<nav aria-label="breadcrumb" class="d-inline-block">
+							<ul class="breadcrumb bg-white rounded shadow mb-0">
+								<li class="breadcrumb-item"><a href="<?= base_url('home'); ?>">Home</a></li>
+								<li class="breadcrumb-item"><a href="javascript:void">Career</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Human Resource</li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
+	</div>
+	<!--end container-->
+</section>
+<!--end section-->
+
+<!-- Shape Start -->
+<div class="position-relative">
+	<div class="shape overflow-hidden text-white">
+		<svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+		</svg>
+	</div>
+</div>
+<!--Shape End-->
+
+
+<!-- How It Work Start -->
+<section class="section bg-light">
+	<div class="container">
+		<div class="row align-items-center">
+			<div class="col-lg-5 col-md-6">
+				<img src="<?= base_url(); ?>assets/images/management/about.jpg" class="img-fluid rounded" alt="">
+			</div>
+			<!--end col-->
+
+			<div class="col-lg-7 col-md-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
+				<div class="section-title ml-lg-5">
+					<h4 class="title mb-4">Human Resource Management</h4>
+					<p class="text-muted">Kami menyadari pentingnya peranan manusia dalam menentukan keberhasilan sebuah
+						organisasi. Oleh karena itu seiring dengan perkembangan organisasi kami, manajemen sumber daya
+						manusia kami memiliki peranan yang semakin penting.
+					</p>
+					<p class="text-muted"> Manajemen sumber daya manusia kami dititikberatkan pada nilai nilai
+						kekeluargaan dan kesejahteraan bersama. Tingkat loyalitas dan semangat kerja yang tinggi serta
+						rendahnya tingkat pergantian tenaga kerja menjadi indikasi suksesnya manajemen sumber daya
+						manusia di organisasi kami.
+					</p>
+					<a href="javascript:void(0)" class="btn btn-primary mt-3">Tentang Kami Selengkapnya <i
+							class="mdi mdi-chevron-right"></i></a>
+				</div>
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
+	</div>
+	<!--end container-->
+
+	<div class="container mt-100 mt-60">
+		<div class="row align-items-center">
+			<div class="col-lg-6 col-md-6 order-2 order-md-1 mt-4 mt-sm-0 pt-2 pt-sm-0">
+				<div class="section-title">
+					<h4 class="title mb-4">Bergabung Bersama<br>PAN ERA GROUP</h4>
+					<p class="text-muted">Kami setiap saat selalu membuka pintu bagi tenaga kerja yang berkualitas.
+						Bergabunglah bersama kami untuk mendapatkan:
+					</p>
+					<ul class="list-unstyled text-muted">
+						<li class="mb-0"><span class="text-primary h5 mr-2"><i
+									class="uim uim-check-circle"></i></span>Lingkungan Kerja Yang Produktif Dan
+							Mendukung Perkembangan Individu
+						</li>
+						<li class="mb-0"><span class="text-primary h5 mr-2"><i
+									class="uim uim-check-circle"></i></span>Upah Yang Menarik
+						</li>
+						<li class="mb-0"><span class="text-primary h5 mr-2"><i
+									class="uim uim-check-circle"></i></span>Potensi Jenjang Karir
+						</li>
+						<li class="mb-0"><span class="text-primary h5 mr-2"><i
+									class="uim uim-check-circle"></i></span>Fleksibilitas Lokasi Kerja
+						</li>
+						<li class="mb-0"><span class="text-primary h5 mr-2"><i
+									class="uim uim-check-circle"></i></span>Pasar Kerja Internal Yang Relatif Stabil
+						</li>
+					</ul>
+					<a href="javascript:void(0)" class="btn btn-primary mt-3">Karir Bersama Kami <i
+							class="mdi mdi-chevron-right"></i></a>
+				</div>
+			</div>
+			<!--end col-->
+
+			<div class="col-lg-6 col-md-6 order-1 order-md-2">
+				<img src="<?= base_url(); ?>assets/images/management/deal-hend.svg" class="img-fluid" alt="">
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
+	</div>
+	<!--end container-->
+
+	<div class="container mt-100 mt-60">
+		<div class="row align-items-center">
+			<div class="col-lg-5 col-md-6">
+				<img src="<?= base_url(); ?>assets/images/management/worker.svg" class="img-fluid" alt="">
+			</div>
+			<!--end col-->
+
+			<div class="col-lg-7 col-md-6 mt-4 mt-sm-0 pt-2 pt-sm-0">
+				<div class="section-title ml-lg-5">
+					<h4 class="title mb-4">Any Question For Us?</h4>
+					<p class="text-muted">Apabila ada pertanyaan mengenai manajemen sumber daya manusia di organisasi
+						kami, silahkan sampaikan ke <a href="mailto:hrd@paneragroup.com">hrd@paneragroup.com</a>.
+					</p>
+					<p class="text-muted">Apabila anda tertarik untuk bergabung dengan kami, silahkan mengunjungi laman
+						<a href="<?= site_url('career'); ?>">kesempatan berkarir bersama Pan Era Group</a>
+					</p>
+					<a href="javascript:void(0)" class="btn btn-primary mt-3">Kontak Kami <i
+							class="mdi mdi-chevron-right"></i></a>
+				</div>
+			</div>
+			<!--end col-->
+		</div>
+		<!--end row-->
+	</div>
+	<!--end container-->
+</section>
+<!--end section-->
