7f254b7e74080a5bc868c58b682a7c32080f21d1
muhadimulyana <muhammadadimulyana30@gmail.com>
2021-03-09 16:38:18 +0700

    change language, content and add timeline

--- application/views/profile/company.php
+++ application/views/profile/company.php
@@ -1,18 +1,31 @@
 <style>
-	@media (max-width: 768px){
+	@media (max-width: 768px) {
 		.bg-half {
 			padding: 100px 0 50px;
 		}
+
+		.bg-timeline {
+		background: url('<?=base_url();?>assets/images/profile/bg-timelines.png') center center; 
+		background-size: cover;
+		}
 	}
+
+	.bg-timeline {
+		background: url('<?=base_url();?>assets/images/profile/bg-timeline.png') center center; 
+		background-size: cover;
+	}
+
 </style>
 
-<section class="d-table w-100 bg-half" style="background: url('<?= base_url(); ?>assets/images/profile/bg-1.jpg') center center; background-size: cover;">
+<section class="d-table w-100 bg-half"
+	style="background: url('<?=base_url();?>assets/images/profile/bg-1.jpg') center center; background-size: cover;">
 	<div class="bg-overlay"></div>
 	<div class="container">
 		<div class="row align-items-center">
 			<div class="col-md-6 col-12 order-2 order-md-1">
 				<div class="title-heading text-white">
-					<h1 style="line-height: 1;" class="font-weight-bold mt-5 mb-3">Profil, Visi & Misi <br> Perusahaan</h1>
+					<h1 style="line-height: 1;" class="font-weight-bold mt-5 mb-3">Profil, Visi & Misi <br> Perusahaan
+					</h1>
 					<p class="para-desc text-white">Pengenalan profil perusahaan dan sejarah dibentuknya Pan Era Group
 					</p>
 				</div>
@@ -23,9 +36,9 @@
 					<div class="page-next">
 						<nav aria-label="breadcrumb" class="d-inline-block">
 							<ul class="breadcrumb bg-white rounded shadow mb-0">
-								<li class="breadcrumb-item"><a href="<?= base_url('home'); ?>">Home</a></li>
-								<li class="breadcrumb-item"><a href="<?= base_url('profile'); ?>">About Us</a></li>
-								<li class="breadcrumb-item active" aria-current="page">Company Profile</li>
+								<li class="breadcrumb-item"><a href="<?=base_url('home');?>">Beranda</a></li>
+								<li class="breadcrumb-item"><a href="<?=base_url('profile');?>">Tentang Kami</a></li>
+								<li class="breadcrumb-item active" aria-current="page">Profil Perusahaan</li>
 							</ul>
 						</nav>
 					</div>
@@ -47,33 +60,94 @@
 			<div class="col-lg-8 col-md-6">
 				<div class="card blog blog-detail border-0 shadow rounded">
 					<div class="card-body content">
-						<p class="text-muted mt-3">Pan Era Group dibentuk pada tahun 2011, dan pada awalnya merupakan
-							gabungan dari dua buah perusahaan, PT Elastis Reka Aktif dan PT Elite Recycling Indonesia.
-						</p>
-						<p class="text-muted">Kami adalah salah satu grup perusahaan terkemuka di Indonesia yang
-							bergerak di dua lini bisnis, yakni pendauran ulang limbah plastik, terutama yang berbahan
-							Polyethylene, dan manufaktur kantong plastik.
-						</p>
-						<p class="text-muted">
-							Kami percaya bahwa daur ulang adalah salah satu solusi utama terhadap masalah keberlanjutan
-							lingkungan hidup (sustainability problem). Dengan kegiatan daur ulang, tidak hanya jumlah
-							limbah yang beredar di lingkungan dapat berkurang, namun juga masyarakat dapat menikmati
-							produk yang diproduksi dengan energi yang lebih rendah. Pengurangan pemakaian energi
-							tersebut adalah kunci utama daripada sebagian besar masalah lingkungan yang ada sekarang.
-							Beberapa contoh di antaranya pemanasan global dan keterbatasan sumber energi. Berbekal dari
-							kepercayaan yang mendalam inilah kami kemudian memfokuskan sumber daya kami ke dalam
-							pengembangan aktivitas daur ulang.
+						<h4>Sejarah</h4>
+						<p class="text-muted mt-3">
+							Pan Era Group berdiri pada tahun 2011, yang bergerak dalam industri plastik. Kegiatan
+							industri plastik yang dijalankan terbagi kedalam dua lini bisnis, yakni produksi bijih
+							plastik daur ulang (recycled plastic) terutama yang berbahan Polyethylene, dan manufaktur
+							kantong plastik. Kegiatan industri ini dijalankan oleh 2 anak perusahaan yaitu PT Elastis
+							Reka Aktif dan PT Elite Recycling Indonesia.
 						</p>
 						<p class="text-muted">
-							Kualitas adalah nilai yang sangat penting dalam organisasi kami. Filosofi kualitas yang
-							dibenamkan ke dalam benak organisasi kami dititikberatkan pada kemampuan dan kewajiban kami
-							untuk melebihi ekspektasi pelanggan, pada setiap saat. Skema pengendalian kualitas kami
-							dirancang sedemikian rupa untuk dapat memenuhi tuntutan tersebut.
+							Kami percaya bahwa kegiatan mendaur ulang plastik adalah salah satu solusi utama dalam
+							menjaga keberlanjutan lingkungan hidup (sustainable development). Melalui kegiatan mendaur
+							ulang plastik, selain mengurangi jumlah limbah yang beredar di lingkungan, masyarakat juga
+							dapat menikmati produk yang berkualitas. Kami berusaha untuk menghasilkan produk - produk
+							terbaik dengan terus mengikuti perkembangan teknologi baik dari mesin produksi maupun dari
+							dunia teknologi informasi. Kualitas adalah nilai yang sangat penting dalam perusahaan kami.
+							Filosofi kualitas yang kami pegang difokuskan pada kemampuan dan kewajiban kami untuk
+							melebihi ekspektasi pelanggan setiap saat.
 						</p>
-						<p class="text-muted">
-							Pada setiap saat kami tidak pernah berhenti untuk terus mengembangkan sistem perusahaan agar
-							sistem manajemen yang berkualitas dapat tercipta.
-						</p>
+						<h4 class="mt-5">Timeline</h4>
+						<div id="customer-testi" class="owl-carousel owl-theme bg-timeline">
+                            <div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">1988</h6></span>
+                                    <p class="text-muted mt-4">PD ERA Plastik didirikan pada tahun 1988 dan mulai melakukan aktivitas jual beli dan pengelolaan limbah plastik</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">1999</h6></span>
+                                    <p class="text-muted mt-4">PD ERA Plastik meluncurkan produk hasil daur ulang yaitu kantong plastik dengan merek dagang "LOCO"</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2004</h6></span>
+                                    <p class="text-muted mt-4">PD ERA Plastik berubah nama menjadi PT. Elastis Reka Aktif dan terdaftar di badan hukum</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2011</h6></span>
+                                    <p class="text-muted mt-4">PT. Elite Recycling Indonesia dibentuk untuk menambah kapasitas produksi kantong plastik merek "LOCO" dan bijih plastik</p>
+                                </div>
+                            </div>
+							
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2011</h6></span>
+                                    <p class="text-muted mt-4">PAN ERA GROUP dibentuk untuk menaungi kedua perusahaan, yaitu PT. Elastis Reka Aktif dan PT. Elite Recycling Indonesia</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2015</h6></span>
+                                    <p class="text-muted mt-4">PT. Elastis Reka Aktif mendapatkan sertifikasi ISO 9001:2008</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2016</h6></span>
+                                    <p class="text-muted mt-4">PT. Elastis Reka Aktif melakukan pembaharuan sertifikasi mejadi ISO 9001:2015</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2017</h6></span>
+                                    <p class="text-muted mt-4">PT. Elite Recycling Indonesia mendapatkan sertifikasi ISO 9001:2015</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2018</h6></span>
+                                    <p class="text-muted mt-4">PT. Elastis Reka Aktif mendapatkan sertifikasi ISO 14001:2015 dalam kategori "Recycling and Manufacture and Recycle of Plastic Bag"</p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2019</h6></span>
+                                    <p class="text-muted mt-4">Kantong Plastik merek "LOCO" mendapatkan verifikasi kesesuaian klaim Ekolabel Indonesia oleh BTP & BPPT </p>
+                                </div>
+                            </div>
+							<div class="card customer-testi border-0 text-center bg-transparent">
+                                <div class="card-body">
+									<span class="badge rounded-pill bg-primary"><h6 class="mb-0 text-white">2020</h6></span>
+                                    <p class="text-muted mt-4">Memperkenalkan dan mendistribusikan desain baru untuk karung dan kemasan dengan logo Ekolabel Indonesia</p>
+                                </div>
+                            </div>
+                        </div>
 					</div>
 				</div>
 
@@ -87,34 +161,34 @@
 
 						<!-- RECENT POST -->
 						<div class="widget mb-4 pb-2">
-							<h5 class="widget-title">About Us</h5>
+							<h5 class="widget-title">Tentang Kami</h5>
 							<div class="mt-4">
 								<div class="clearfix post-recent">
 									<div class="post-recent-thumb float-left" style="width: 18%;"> <a
-											href="<?= site_url('profile/company'); ?>"> <img alt="img" width="50"
-												src="<?= base_url(); ?>assets/images/profile/building.svg"
+											href="<?=site_url('profile/company');?>"> <img alt="img" width="50"
+												src="<?=base_url();?>assets/images/profile/building.svg"
 												class="img-fluid rounded"></a></div>
 									<div class="post-recent-content float-left"><a
-											href="<?= site_url('profile/company'); ?>">Company
-											Profile</a><span class="text-muted mt-2">About Us </span></div>
+											href="<?=site_url('profile/company');?>">Company
+											Profile</a><span class="text-muted mt-2">Tentang Kami </span></div>
 								</div>
 								<div class="clearfix post-recent">
 									<div class="post-recent-thumb float-left" style="width: 18%;"> <a
-											href="<?= site_url('profile/era'); ?>"> <img alt="img" width="50"
-												src="<?= base_url(); ?>assets/images/profile/era-icon.png"
+											href="<?=site_url('profile/era');?>"> <img alt="img" width="50"
+												src="<?=base_url();?>assets/images/profile/era-icon.png"
 												class="img-fluid rounded"></a></div>
 									<div class="post-recent-content float-left"><a
-											href="<?= site_url('profile/era'); ?>">PT. Elastis
-											Reka Aktif</a> <span class="text-muted mt-2">About Us </span></div>
+											href="<?=site_url('profile/era');?>">PT. Elastis
+											Reka Aktif</a> <span class="text-muted mt-2">Tentang Kami </span></div>
 								</div>
 								<div class="clearfix post-recent">
 									<div class="post-recent-thumb float-left" style="width: 18%;"> <a
-											href="<?= site_url('profile/eri'); ?>"> <img alt="img" width="50"
-												src="<?= base_url(); ?>assets/images/profile/eri-icon.png"
+											href="<?=site_url('profile/eri');?>"> <img alt="img" width="50"
+												src="<?=base_url();?>assets/images/profile/eri-icon.png"
 												class="img-fluid rounded"></a></div>
 									<div class="post-recent-content float-left"><a
-											href="<?= site_url('profile/eri'); ?>">PT. Elite
-											Recycling Indonesia</a> <span class="text-muted mt-2">About Us </span>
+											href="<?=site_url('profile/eri');?>">PT. Elite
+											Recycling Indonesia</a> <span class="text-muted mt-2">Tentang Kami </span>
 									</div>
 								</div>
 							</div>
